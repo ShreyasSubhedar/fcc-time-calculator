6251906d0b895e6d1a5f0e2dc74610cd54a0b927
Shreyas Subhedar <shreyassubhedar@gmail.com>
2020-09-03 04:58:31 +0000

    Beautify code ❤️

--- time_calculator.py
+++ time_calculator.py
@@ -1,75 +1,75 @@
-def add_time(start, duration,day=None):
-  # Maintaining Days in a week
-  day_map ={
-    "Saturday":0,
-    "Sunday":1,
-    "Monday":2,
-    "Tuesday":3,
-    "Wednesday":4,
-    "Thursday":5,
-    "Friday":6
-  }
-  # Getting useful data from start string
-  timely,midday= start.split()
-  hour,minutes = timely.split(':')
-  hour= int(hour)
-  minutes = int(minutes)
+def add_time(start, duration, day=None):
+    # Maintaining Days in a week
+    day_map = {
+        "Saturday": 0,
+        "Sunday": 1,
+        "Monday": 2,
+        "Tuesday": 3,
+        "Wednesday": 4,
+        "Thursday": 5,
+        "Friday": 6
+    }
+    # Getting useful data from start string
+    timely, midday = start.split()
+    hour, minutes = timely.split(':')
+    hour = int(hour)
+    minutes = int(minutes)
 
-  # Making the clock into 24 hour format
-  if midday == "PM":
-    hour += 12
-  
-  # Getting data from duration 
-  duration_hour,duration_minutes = duration.split(':')
-  duration_hour = int(duration_hour)
-  duration_minutes = int(duration_minutes)
+    # Making the clock into 24 hour format
+    if midday == "PM":
+        hour += 12
 
-  # Calculating total hours, minutes 
-  total_minutes = minutes + duration_minutes
-  ans_minutes = total_minutes % 60
-  extra_hours = total_minutes // 60
-  total_hour = hour+ duration_hour +extra_hours
+    # Getting data from duration
+    duration_hour, duration_minutes = duration.split(':')
+    duration_hour = int(duration_hour)
+    duration_minutes = int(duration_minutes)
 
-  # final hours as per 12 Hour clock
-  ans_hour = (total_hour % 24) % 12
+    # Calculating total hours, minutes
+    total_minutes = minutes + duration_minutes
+    ans_minutes = total_minutes % 60
+    extra_hours = total_minutes // 60
+    total_hour = hour + duration_hour + extra_hours
 
-  # Edge case
-  if ans_hour == 0:
-    ans_hour =12
-  ans_hour =str(ans_hour)
+    # final hours as per 12 Hour clock
+    ans_hour = (total_hour % 24) % 12
 
-  # total days 24 hr 1 day
-  total_day = (total_hour // 24)
+    # Edge case
+    if ans_hour == 0:
+        ans_hour = 12
+    ans_hour = str(ans_hour)
 
-  # deciding mid day (AM/PM)
-  ans_midday=""
-  if (total_hour%24) <=11:
-    ans_midday="AM"
-  else:
-    ans_midday="PM"
+    # total days 24 hr 1 day
+    total_day = (total_hour // 24)
 
-  # Handling single digit minutes case
-  if ans_minutes<=9:
-    ans_minutes = '0'+str(ans_minutes)
-  else:
-    ans_minutes = str(ans_minutes)
-  # returning logic
-  time_stamp = ans_hour+":"+ans_minutes+' '+ans_midday 
-  if day == None:
-    if total_day == 0 :
-      return  time_stamp
-    if total_day == 1:
-      return time_stamp +' (next day)'
-    return time_stamp+' ('+ str(total_day)+' days later)'
-  else:
-    ans_day = (day_map[day.lower().capitalize()]+total_day)%7
-    for i, j in day_map.items():
-      if j == ans_day:
-        ans_day= i
-        break
-    if total_day == 0 :
-      return time_stamp +', '+ans_day
-    if total_day == 1:
-      return time_stamp +', '+ans_day+' (next day)'
-    return time_stamp+', '+ans_day+' ('+ str(total_day)+' days later)'
+    # deciding mid day (AM/PM)
+    ans_midday = ""
+    if (total_hour % 24) <= 11:
+        ans_midday = "AM"
+    else:
+        ans_midday = "PM"
 
+    # Handling single digit minutes case
+    if ans_minutes <= 9:
+        ans_minutes = '0' + str(ans_minutes)
+    else:
+        ans_minutes = str(ans_minutes)
+    # returning logic
+    time_stamp = ans_hour + ":" + ans_minutes + ' ' + ans_midday
+    if day == None:
+        if total_day == 0:
+            return time_stamp
+        if total_day == 1:
+            return time_stamp + ' (next day)'
+        return time_stamp + ' (' + str(total_day) + ' days later)'
+    else:
+        ans_day = (day_map[day.lower().capitalize()] + total_day) % 7
+        for i, j in day_map.items():
+            if j == ans_day:
+                ans_day = i
+                break
+        if total_day == 0:
+            return time_stamp + ', ' + ans_day
+        if total_day == 1:
+            return time_stamp + ', ' + ans_day + ' (next day)'
+        return time_stamp + ', ' + ans_day + ' (' + str(
+            total_day) + ' days later)'
