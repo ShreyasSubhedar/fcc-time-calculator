d06e3a196afd2a9958892950df460c9bc8a49b5f
Shreyas Subhedar <shreyassubhedar@gmail.com>
2020-09-03 03:34:25 +0000

    Logic without optional parameter

--- time_calculator.py
+++ time_calculator.py
@@ -10,7 +10,34 @@ def add_time(start, duration,day="Anyday"):
   
   # Getting data from duration 
   duration_hour,duration_minutes = duration.split(':')
+  duration_hour = int(duration_hour)
+  duration_minutes = int(duration_minutes)
 
+  # Calculating total hours, minutes 
+  total_minutes = minutes + duration_minutes
+  ans_minutes = total_minutes % 60
+  extra_hours = total_minutes // 60
+  total_hour = hour+ duration_hour +extra_hours
+  # final hours as per 12 Hour clock
+  ans_hour = (total_hour % 24) % 12
+  if ans_hour == 0:
+    ans_hour =12
+  # 
+  total_day = (total_hour // 24)
+  # deciding mid day
+  ans_midday=""
+  if (total_hour%24) <=11:
+    ans_midday="AM"
+  else:
+    ans_midday="PM"
+  if ans_minutes<=9:
+    ans_minutes = '0'+str(ans_minutes)
+  else:
+    ans_minutes = str(ans_minutes)
 
-  
-  return None
+  if total_day == 0 :
+    return str(ans_hour)+":"+ans_minutes+' '+ans_midday 
+  if total_day == 1:
+    return str(ans_hour)+":"+ans_minutes+' '+ans_midday +' (next day)'
+  ans = str(ans_hour)+":"+ans_minutes+' '+ans_midday+' ('+ str(total_day)+' days later)'
+  return ans
